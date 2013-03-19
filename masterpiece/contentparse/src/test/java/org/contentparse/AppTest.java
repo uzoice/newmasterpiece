6e6c5bd183876b4384447f992ef0df72774760be
uzoice <uzoice@163.com>
2013-03-19 12:51:14 +0800

    init

---
+++ masterpiece/contentparse/src/test/java/org/contentparse/AppTest.java
@@ -0,0 +1,21 @@
+package org.contentparse;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest {
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	public void testApp() {
+		System.out.println("hello_parse");
+		assertTrue(true);
+	}
+}
